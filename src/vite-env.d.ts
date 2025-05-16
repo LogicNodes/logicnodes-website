43b3ad9456eab17987c788ca89c1134784a22ab9
Rasmus Raundahl <Rasmusraundahl01@gmail.com>
2025-05-16 10:37:22 +0200

    Bugfixes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,13 @@
 /// <reference types="astro/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 declare module '*.svg?component' {
+  import type { ComponentType, SVGProps } from 'react';
+  const component: ComponentType<SVGProps<SVGSVGElement>>;
+  export default component;
+}
+
+declare module '*.svg?react' {
   import type { ComponentType, SVGProps } from 'react';
   const component: ComponentType<SVGProps<SVGSVGElement>>;
   export default component;
