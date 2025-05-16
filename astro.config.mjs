ffe59da8d1b017cc795baba1a368350061e79764
Rasmus Raundahl <Rasmusraundahl01@gmail.com>
2025-05-16 09:07:27 +0200

    Moved SVG's to SRC

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@
 
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import svgr from 'vite-plugin-svgr';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 import react from '@astrojs/react';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: { 
+    plugins: [svgr()],
+    resolve: { 
+      alias: { 
+        '@': path.resolve(__dirname, './src')
+      } 
+    }
+  }
 });
--- src/icons/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Zap } from './Zap.astro';
-export { default as ChartPie } from './ChartPie.astro';
-export { default as BanknoteArrowDown } from './BanknoteArrowDown.astro';
